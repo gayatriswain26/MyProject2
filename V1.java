ed4ea0fc99b6c48c194bc93708cb7bc69e7cea6d
gayatriswain26 <gayatriswain061@gmail.com>
2024-02-15 12:13:43 +0530

    Added successfully

---
+++ V1.java
@@ -0,0 +1,6 @@
+public class V1{
+
+public static void main(String[] args){
+    System.out.println("Welcome to Git and GitHub");
+}
+}
---
+++ V2.java
@@ -0,0 +1,6 @@
+public class V2{
+
+public static void main(String[] args){
+    System.out.println("Welcome to Git and GitHub");
+}
+}
